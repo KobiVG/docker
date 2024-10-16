1ac05e7dd69418bb56ca705d502a42650960120e
KobiVG <kobevangestel@outlook.com>
2024-10-16 17:04:45 +0200

    docker compose

--- docker-compose/app.py
+++ docker-compose/app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World from flask! I have been seen {} times.\n'.format(count)
+    return 'Sup cuh from flask! I have been seen {} times.\n'.format(count)
